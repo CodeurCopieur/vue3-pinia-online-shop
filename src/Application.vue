fdc23274c661e1ac58ccb7a6632b55b667888f5e
Widdy LOUIS <louisws@outlook.fr>
2023-09-20 14:23:46 +0200

    Update Application.vue

--- src/Application.vue
+++ src/Application.vue
@@ -2,9 +2,5 @@
 </script>
 
 <template>
-          <router-view v-slot="{ Component }">
-            <component :is="Component"></component>
-          </router-view>
-
-          <!-- <h1>home</h1> -->
+ <RouterView/>
 </template>
