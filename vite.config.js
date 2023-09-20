66d6af240fa227c830b02fc9942a9e1cc3ec97c6
Widdy LOUIS <louisws@outlook.fr>
2023-09-20 16:54:19 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import UnoCss from 'unocss/vite'
 import {presetAttributify, presetUno} from 'unocss'
 
+import { fileURLToPath, URL } from 'node:url'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), UnoCss([{ presets: [presetUno(), presetAttributify()] }])],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
