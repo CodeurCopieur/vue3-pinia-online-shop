7eedaac765e379ea0fc65eff91f6e71bc74a85d1
Widdy LOUIS <louisws@outlook.fr>
2023-09-22 23:15:01 +0200

    Update ProductList.vue

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,13 +1,15 @@
 <script setup>
     /* imports */
+
     import { storeToRefs } from 'pinia'
     import { useProductStore } from '@/store/productStore'
     import useProducts from '@/composables/useProducts';
-    /* imports */
-  import CityCardSkeleton from '@/components/CityCardSkeleton.vue'
-    
+    import CityCardSkeleton from '@/components/CityCardSkeleton.vue'
+    import Product from '@/components/Product.vue'
 
+    /* composables */
     const { products } = useProducts();
+    /* store */
     const productStore  = useProductStore();
 
     const { loading } = storeToRefs(productStore);
@@ -20,7 +22,7 @@
   <template v-if="loading">
     <CityCardSkeleton
       class=" h-[350px] rounded-md shadow-md overflow-hidden"
-      v-for="(product, i) in products" :key="i" />
+      v-for="product in products.length" :key="product" />
   </template>
 
   <template v-else>
@@ -28,16 +30,7 @@
       class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden" 
       v-for="product in products" 
       :key="product.id">
-      <div class="flex items-end justify-end h-56 w-full bg-cover bg-center" :style="{backgroundImage: 'url('+ product.thumbnail +')'}">
-        <button 
-          class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500 border-none cursor-pointer">
-            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
-        </button>
-      </div>
-      <div class="px-5 py-3">
-          <h3 class="text-gray-700 uppercase">{{ product.title }}</h3>
-          <span class="text-gray-500 mt-2">${{ product.price }}</span>
-      </div>
+      <Product :product="product"/>
     </div>
   </template>
 </template>
