a98f424638593b5888afc4f264d7cb7683b4f93f
Widdy LOUIS <louisws@outlook.fr>
2023-09-22 16:28:28 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <script setup>
 
+  /* imports */
+  import ProductList from '@/components/ProductList.vue';
+
 </script>
 
 <template>
- <div class="container">
-  <h1>Home</h1>
+ <div class="mt-16">
+  <h3 class="text-gray-600 text-2xl font-medium">Catalogue</h3>
+  <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+    <ProductList />
+  </div>
  </div>
 </template>
