975ef6674fb6b1b075c1e77137e3feeddb9bdec2
Widdy LOUIS <louisws@outlook.fr>
2023-09-25 21:05:17 +0200

    Update Application.vue

--- src/Application.vue
+++ src/Application.vue
@@ -1,9 +1,19 @@
-<template>
+<script setup>
 
-    <main class="my-8">
-      <div class="container mx-auto">
-        <RouterView/>
-      </div>
-    </main>
- 
+  import { onMounted } from 'vue'
+  import { useProductStore } from '@/store/productStore'
+  import useProducts from '@/composables/useProducts';
+
+  onMounted(() => {
+    /* composables */
+    useProductStore().getProducts();
+    /* store */
+    // useProductStore().getProducts();
+  });
+
+</script>
+<template>
+  <main class="my-8">
+      <RouterView/>
+  </main>
 </template>
