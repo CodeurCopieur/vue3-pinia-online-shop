b6206bb51e9d9279723af87b2351486a984f181d
Widdy LOUIS <louisws@outlook.fr>
2023-09-25 21:05:25 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import ProductDetail from '@/views/ProductDetail.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const routes = [
   {path: '/', name: 'Home', component: Home},
   {path: '/product/:id', name: 'ProductView', component: ProductDetail},
+  {path: '/:NotFound(.*)*',name: 'NotFound',component: NotFound}
 ]
 
 const router = createRouter({
