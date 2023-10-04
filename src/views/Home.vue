4f9bf14a93d7cea8dc17fec7a315a71e2baa6066
Widdy LOUIS <louisws@outlook.fr>
2023-10-04 19:38:25 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
 
 <template>
   
-  <div class="container mx-auto">
+  <div class="max-w-7xl mx-auto">
     <div class="mt-16">
       <h3 class="text-gray-600 text-2xl font-medium">Catalogue</h3>
       <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
