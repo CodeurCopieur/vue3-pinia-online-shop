2a0f35693f176db91d5c7da6b491f09c37d17a7c
Widdy LOUIS <louisws@outlook.fr>
2023-10-06 20:18:19 +0200

    Update Product.vue

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -4,12 +4,32 @@
       type: Object
     }
   });
+
+  import { useProductStore } from '@/store/productStore'
+
+  const store = useProductStore()
+
+  const addToCart = () => {
+    console.log('test');
+  };
+
+  const isInBag = (product) => {
+    return store.Cart.find( item => item.id == product.id)
+   };
 </script>
 
 <template>
   <div class="flex items-end justify-end h-56 w-full bg-cover bg-center" :style="{backgroundImage: 'url('+ product.thumbnail +')'}">
     <button 
-      class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500 border-none cursor-pointer pointer-events-unset" @click.stop="console.log('oui')">
+    v-if="!isInBag(product)"
+      @click.stop="addToCart"
+      class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500 border-none cursor-pointer pointer-events-unset">
+        <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+    </button>
+    <button 
+      v-else
+      
+      class="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500 border-none cursor-pointer pointer-events-unset">
         <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
     </button>
   </div>
