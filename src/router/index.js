d75cf24519959e7f3ff736e63b6c41b3b5501ab9
Widdy LOUIS <louisws@outlook.fr>
2023-10-06 18:34:12 +0200

    add new route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import ProductDetail from '@/views/ProductDetail.vue'
+import Cart from '@/views/Cart.vue'
 import NotFound from '@/views/NotFound.vue'
 
 const routes = [
   {path: '/', name: 'Home', component: Home},
   {path: '/product/:id', name: 'ProductView', component: ProductDetail},
+  {path: '/cart', name: 'CartView', component: Cart},
   {path: '/:NotFound(.*)*',name: 'NotFound',component: NotFound}
 ]
 
