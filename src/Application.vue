84ed4797bd24ded620ffc207ca33d251d0f38979
Widdy LOUIS <louisws@outlook.fr>
2023-10-06 18:57:03 +0200

    Update Application.vue

--- src/Application.vue
+++ src/Application.vue
@@ -2,7 +2,8 @@
 
   import { onMounted } from 'vue'
   import { useProductStore } from '@/store/productStore'
-  // import useProducts from '@/composables/useProducts';
+  
+  const store = useProductStore()
 
   onMounted(() => {
     /* composables */
@@ -13,6 +14,10 @@
 
 </script>
 <template>
+  <header id="header" class="flex justify-center">
+    <router-link to="/" class="mr-4 no-underline">Home</router-link> 
+    <router-link to="/cart" class="no-underline">articles dans le panier : ({{ store.Cart.length }}) </router-link> 
+  </header>
   <main class="my-8">
       <RouterView/>
   </main>
