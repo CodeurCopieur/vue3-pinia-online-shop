ec6291c1e75d06b257e281f97c8472d4ed6cfc4c
Widdy LOUIS <louisws@outlook.fr>
2023-10-07 18:49:16 +0200

    Update Product.vue

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -9,8 +9,8 @@
 
   const store = useProductStore()
 
-  const addToCart = () => {
-    console.log('test');
+  const addToCart = (product) => {
+    store.addToCart(product)
   };
 
   const isInBag = (product) => {
@@ -22,7 +22,7 @@
   <div class="flex items-end justify-end h-56 w-full bg-cover bg-center" :style="{backgroundImage: 'url('+ product.thumbnail +')'}">
     <button 
     v-if="!isInBag(product)"
-      @click.stop="addToCart"
+      @click.stop="addToCart(product)"
       class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500 border-none cursor-pointer pointer-events-unset">
         <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
     </button>
