e02d5d925ff7a3257147d31a9fbb5a97757ee346
Widdy LOUIS <louisws@outlook.fr>
2023-10-07 17:26:00 +0200

    Create Cart.vue

---
+++ src/views/Cart.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import { useProductStore } from '@/store/productStore'
+  import { useRoute, useRouter } from 'vue-router';
+
+  const store = useProductStore()
+  const router = useRouter();
+
+</script>
+
+<template>
+  <div class="max-w-7xl mx-auto">
+    <div class="mt-16">
+      <h3 class="text-gray-600 text-2xl font-medium">Panier</h3>
+    </div>
+  </div>
+
+</template>
